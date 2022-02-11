b414731776f33637f498940baf24bf168388e705
sujatann <naikarsujata565@gmail.com>
2022-02-11 15:07:42 +0530

    adding information to milestone-1,#1

--- 0_Certificates/1_Requirements/2_Architecture/3_Implementation/simplecalculator.c
+++ 0_Certificates/1_Requirements/2_Architecture/3_Implementation/simplecalculator.c
@@ -1,5 +1,5 @@
 /**
- * @file Simplecalculator.C
+ * @file Simple Calculator.C
  * @author sujata ()
  * @brief simple computer:add,sub,mul,div,sqr,cube
  * @version 0.1
@@ -9,26 +9,43 @@
  * 
  */
 //simple computer:add,sub,mul,div,sqr,cube
+
+// progarm to obtain some basic operations used in simple calculators
+
 #include<stdio.h>
+
+#include<stdlib.h>
+
+#include<math.h>
 
 int main()
 {
    int choose;
    long no1, no2, m;
-   printf("Please choose your option:"
-          "\n1 = Addition"
-          "\n2 = Subtraction"
-          "\n3 = Multiplication"
-          "\n4 = Division"
-          "\n5 = Squares"
-          "\n6 = Cubes"
-          "\n7 = exit"
-          "\n\nChoice: ");
+
+   printf(" select any one from the  options  below:"
+
+          "\n1 = Addition\n"
+
+          "\n2 = Subtraction\n"
+
+          "\n3 = Multiplication\n"
+
+          "\n4 = Division\n"
+
+          "\n5 = Squares\n"
+ 
+          "\n6 = Cubes\n"
+
+          "\n7 = exit\n"
+
+          "\n\nchoose: ");
+
    scanf("%d", &choose);
-   //while loop check whether the choice is in the given range
+   //while loop check whether the choose option  is in the given range
    while(choose< 1 || choose > 7)
    {
-      printf("\nPlease choose the above mentioned option."
+      printf("\nchoose the above mentioned option."
              "\nChoose: ");
       scanf("%d", &choose);
    }
@@ -36,38 +53,42 @@ int main()
    {
 
    case 1:
-      printf("Enter two numbers: \n");
+      printf(" Please enter  any  two numbers to add: \n");
+
       scanf("%ld %ld", &no1, &no2);
       m = no1 + no2;
       printf("Sum = %ld", m);
       break;
 
    case 2:
-      printf("Enter two numbers: \n");
+      printf("Please enter any  two numbers  to substract: \n");
       scanf("%ld %ld", &no1, &no2);
       m = no1 - no2;
-      printf("Subtraction = %ld", m);
+      printf("Sub = %ld", m);
       break;
 
    case 3:
 
-      printf("Enter two numbers: \n");
+      printf("Please enter any two numbers to multiply: \n");
       scanf("%ld %ld", &no1, &no2);
       m = no1 * no2;
-      printf("Product = %ld", m);
+      printf("Pdt= %ld", m);
       break;
    case 4:
 
-      printf("Enter Dividend: ");
+      printf("Enter the Dividend: ");
+
       scanf("%ld", &no1);
-      printf("Enter Divisor: ");
+
+      printf("Enter the Divisor: ");
+
       scanf("%ld", &no2);
-     //while loop checks for divisor whether it is zero or not
+     //while loop checks for  the divisor whether it is zero or not
 
      while(no2 == 0)
      {
         printf("\nDivisor cannot be zero."
-               "\nEnter divisor once again: ");
+               "\nEnter divisor once again to confirm: ");
         scanf("%ld", &no2);
      }
 
@@ -76,17 +97,21 @@ int main()
      break;
 
    case 5:
-      printf("Enter any number: \n");
+      printf("Please enter any number of your choice: \n");
       scanf("%ld", &no1);
       m= no1 * no1;
-      printf("Square = %ld", m);
+      printf("Sqr = %ld", m);
       break;
 
-   case 6:printf("Enter any number: \n");
-      scanf("%ld", &no1);
-      m = no1 * no1 * no1;
-      printf("Cube = %ld", m);1
+   case 6:
    
+      printf("Please enter any number of  your choice: \n");
+
+      scanf("%ld", &no1);
+
+      m = no1 * no1 * no1;
+      printf("Cube = %ld", m);
+
       break;
 
    case 7:
