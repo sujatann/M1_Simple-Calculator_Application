9b51585c96aa70a580fc2f3589b7a864bea0938b
sujatann <naikarsujata565@gmail.com>
2022-02-12 10:39:37 +0530

    adding to milestone-1,#1

--- 0_Certificates/1_Requirements/2_Architecture/3_Implementation/simplecalculator.h
+++ 0_Certificates/1_Requirements/2_Architecture/3_Implementation/simplecalculator.h
@@ -1,13 +1,5 @@
-/**
- * @file simplecalculator.h
- * @author sujata()
- * @brief 
- * @version 0.1
- * @date 2022-02-05
- * 
- * @copyright Copyright (c) 2022
- * 
- */
+
+// include files
 
  # include<stdio.h>
 
